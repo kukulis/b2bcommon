d7e4a008214520595854b0672ed67c2631351035
Tomas Barauskas <tomas.barauskas@transol.lt>
2023-08-25 09:23:45 +0300

    467440: add functionality for exporting products to pigu.lt

--- src/Catalog/B2b/Common/Data/Catalog/Product.php
+++ src/Catalog/B2b/Common/Data/Catalog/Product.php
@@ -8,50 +8,311 @@
 
 namespace Catalog\B2b\Common\Data\Catalog;
 
+use JMS\Serializer\Annotation as Serializer;
+use DateTime;
+use Sketis\Kainorasciai\Entity\KainorascioPrekeEntity;
 
 class Product
 {
+    /**
+     * @var Category[]
+     * @Serializer\Type("array<Catalog\B2b\Common\Data\Catalog\Category>")
+     */
+    public $categories;
+
+    /**
+     * @var string
+     * @Serializer\Type("string")
+     */
     public $sku;
+
+    /**
+     * @var DateTime
+     * @Serializer\Type("DateTime<'Y-m-d H:i:s'>")
+     */
     public $lastUpdate;
-    public $version=0;
+
+    /**
+     * @var int
+     * @Serializer\Type("integer")
+     */
+    public $version = 0;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $brand;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $line;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $parentSku;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $originCountryCode;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $vendor;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $manufacturer;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $type;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $purpose;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $measure;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $color;
-    public $forMale=false;
-    public $forFemale=false;
+
+    /**
+     * @var bool
+     * @Serializer\Type("boolean")
+     */
+    public $forMale = false;
+
+    /**
+     * @var bool
+     * @Serializer\Type("boolean")
+     */
+    public $forFemale = false;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $size;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $packSize;
+
+    /**
+     * @var int|null
+     * @Serializer\Type("integer")
+     */
     public $packAmount;
-    public $weight=0;
-    public $length=0;
-    public $height=0;
-    public $width=0;
+
+    /**
+     * @var float
+     * @Serializer\Type("float")
+     */
+    public $weight = 0;
+
+    /**
+     * @var float
+     * @Serializer\Type("float")
+     */
+    public $length = 0;
+
+    /**
+     * @var float
+     * @Serializer\Type("float")
+     */
+    public $height = 0;
+
+    /**
+     * @var float
+     * @Serializer\Type("float")
+     */
+    public $width = 0;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $deliveryTime;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $depositCode;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $codeFromCustom;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $guaranty;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $codeFromSupplier;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $codeFromVendor;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $productgroup;
+
+    /**
+     * @var int|null
+     * @Serializer\Type("integer")
+     */
     public $priority;
-    public $googleProductCategoryId=0;
+
+    /**
+     * @var int
+     * @Serializer\Type("integer")
+     */
+    public $googleProductCategoryId = 0;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $ean;
 
-    // --
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $language;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $name;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $description;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $label;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $variantName;
+
+    /**
+     * @Serializer\Type("string")
+     * @Serializer\Accessor(getter="getTagsAsString",setter="setTagsFromString")
+     */
     public $tags;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $labelSize;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $distributor;
+
+    /**
+     * @var string|null
+     * @Serializer\Type("string")
+     */
     public $composition;
+
+    /**
+     * @var KainorascioPrekeEntity|null
+     */
+    private $pricelistProduct;
+
+    /**
+     * @var int
+     */
+    private $stock = 0;
+
+    /**
+     * Return the tags as a comma-separated string
+     *
+     * @return string
+     */
+    public function getTagsAsString(): string
+    {
+        return implode(',', $this->tags);
+    }
+
+    /**
+     * Set the tags from a comma-separated string or an empty string
+     *
+     * @param string $tagsString
+     */
+    public function setTagsFromString(string $tagsString)
+    {
+        if (empty($tagsString)) {
+            $this->tags = [];
+        } else {
+            $this->tags = explode(',', $tagsString);
+        }
+    }
+
+    public function getPricelistProduct(): ?KainorascioPrekeEntity {
+        return $this->pricelistProduct;
+    }
+
+    public function setPricelistProduct(?KainorascioPrekeEntity $pricelistProduct): void {
+        $this->pricelistProduct = $pricelistProduct;
+    }
+
+    public function getStock(): int {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): void {
+        $this->stock = $stock;
+    }
 }
