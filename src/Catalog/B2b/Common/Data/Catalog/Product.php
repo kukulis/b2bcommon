c4f607c2a56f3d0e5a303234b4cafd8c01040a5d
Giedrius Tumelis <giedrius@inkodus.lt>
2024-07-12 14:38:46 +0300

    #dev-427 : phpunit setting.

--- src/Catalog/B2b/Common/Data/Catalog/Product.php
+++ src/Catalog/B2b/Common/Data/Catalog/Product.php
@@ -295,28 +295,286 @@ class Product
     /**
      * Set the tags from a comma-separated string or an empty string
      */
-    public function setTagsFromString(?string $tagsString)
+    public function setTagsFromString(?string $tagsString): self
     {
         if (empty($tagsString)) {
             $this->tags = [];
         } else {
             $this->tags = explode(',', $tagsString);
         }
+
+        return $this;
     }
 
     public function getPricelistProduct() {
         return $this->pricelistProduct;
     }
 
-    public function setPricelistProduct($pricelistProduct): void {
+    public function setPricelistProduct($pricelistProduct): self {
         $this->pricelistProduct = $pricelistProduct;
+
+        return $this;
     }
 
     public function getStock(): int {
         return $this->stock;
     }
 
-    public function setStock(int $stock): void {
+    public function setStock(int $stock): self {
         $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function setCategories(array $categories): Product
+    {
+        $this->categories = $categories;
+        return $this;
+    }
+
+    public function setSku(string $sku): Product
+    {
+        $this->sku = $sku;
+        return $this;
+    }
+
+    public function setLastUpdate(DateTime $lastUpdate): Product
+    {
+        $this->lastUpdate = $lastUpdate;
+        return $this;
+    }
+
+    public function setVersion(int $version): Product
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    public function setBrand(?string $brand): Product
+    {
+        $this->brand = $brand;
+        return $this;
+    }
+
+    public function setLine(?string $line): Product
+    {
+        $this->line = $line;
+        return $this;
+    }
+
+    public function setParentSku(?string $parentSku): Product
+    {
+        $this->parentSku = $parentSku;
+        return $this;
+    }
+
+    public function setOriginCountryCode(?string $originCountryCode): Product
+    {
+        $this->originCountryCode = $originCountryCode;
+        return $this;
+    }
+
+    public function setVendor(?string $vendor): Product
+    {
+        $this->vendor = $vendor;
+        return $this;
+    }
+
+    public function setManufacturer(?string $manufacturer): Product
+    {
+        $this->manufacturer = $manufacturer;
+        return $this;
+    }
+
+    public function setType(?string $type): Product
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function setPurpose(?string $purpose): Product
+    {
+        $this->purpose = $purpose;
+        return $this;
+    }
+
+    public function setMeasure(?string $measure): Product
+    {
+        $this->measure = $measure;
+        return $this;
+    }
+
+    public function setColor(?string $color): Product
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+    public function setForMale(bool $forMale): Product
+    {
+        $this->forMale = $forMale;
+        return $this;
+    }
+
+    public function setForFemale(bool $forFemale): Product
+    {
+        $this->forFemale = $forFemale;
+        return $this;
+    }
+
+    public function setSize(?string $size): Product
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    public function setPackSize(?string $packSize): Product
+    {
+        $this->packSize = $packSize;
+        return $this;
+    }
+
+    public function setPackAmount(?int $packAmount): Product
+    {
+        $this->packAmount = $packAmount;
+        return $this;
+    }
+
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+        return $this;
+    }
+
+    public function setWeightBruto($weightBruto)
+    {
+        $this->weightBruto = $weightBruto;
+        return $this;
+    }
+
+    public function setLength($length)
+    {
+        $this->length = $length;
+        return $this;
+    }
+
+    public function setHeight($height)
+    {
+        $this->height = $height;
+        return $this;
+    }
+
+    public function setWidth($width)
+    {
+        $this->width = $width;
+        return $this;
+    }
+
+    public function setDeliveryTime(?string $deliveryTime): Product
+    {
+        $this->deliveryTime = $deliveryTime;
+        return $this;
+    }
+
+    public function setDepositCode(?string $depositCode): Product
+    {
+        $this->depositCode = $depositCode;
+        return $this;
+    }
+
+    public function setCodeFromCustom(?string $codeFromCustom): Product
+    {
+        $this->codeFromCustom = $codeFromCustom;
+        return $this;
+    }
+
+    public function setGuaranty(?string $guaranty): Product
+    {
+        $this->guaranty = $guaranty;
+        return $this;
+    }
+
+    public function setCodeFromSupplier(?string $codeFromSupplier): Product
+    {
+        $this->codeFromSupplier = $codeFromSupplier;
+        return $this;
+    }
+
+    public function setCodeFromVendor(?string $codeFromVendor): Product
+    {
+        $this->codeFromVendor = $codeFromVendor;
+        return $this;
+    }
+
+    public function setProductgroup(?string $productgroup): Product
+    {
+        $this->productgroup = $productgroup;
+        return $this;
+    }
+
+    public function setPriority(?int $priority): Product
+    {
+        $this->priority = $priority;
+        return $this;
+    }
+
+    public function setGoogleProductCategoryId(int $googleProductCategoryId): Product
+    {
+        $this->googleProductCategoryId = $googleProductCategoryId;
+        return $this;
+    }
+
+    public function setEan(?string $ean): Product
+    {
+        $this->ean = $ean;
+        return $this;
+    }
+
+    public function setLanguage(?string $language): Product
+    {
+        $this->language = $language;
+        return $this;
+    }
+
+    public function setName(?string $name): Product
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setDescription(?string $description): Product
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function setLabel(?string $label): Product
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    public function setVariantName(?string $variantName): Product
+    {
+        $this->variantName = $variantName;
+        return $this;
+    }
+
+    public function setLabelSize(?string $labelSize): Product
+    {
+        $this->labelSize = $labelSize;
+        return $this;
+    }
+
+    public function setDistributor(?string $distributor): Product
+    {
+        $this->distributor = $distributor;
+        return $this;
+    }
+
+    public function setComposition(?string $composition): Product
+    {
+        $this->composition = $composition;
+        return $this;
     }
 }
